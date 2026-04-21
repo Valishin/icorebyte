6d800e5a43c820ebd04fad8f1b3df68a3551a7ba
Alex Valishin <abubekirov.alex@gmail.com>
2026-04-21 20:33:08 +0200

    feat: added new icons

--- resources/assets/icons/index.ts
+++ resources/assets/icons/index.ts
@@ -1,11 +1,20 @@
+import IconCable from './cable.svg'
+import IconCartridge from './cartridge.svg'
 import IconDesktop from './desktop.svg'
 import IconElectronic from './electronic.svg'
+import IconHarddisk from './harddisk.svg'
 import IconLaptop from './laptop.svg'
+import IconLink from './link.svg'
+import IconLocation from './location.svg'
 import IconMaintenance from './maintenance.svg'
 import IconMobile from './mobile.svg'
+import IconPhone from './phone.svg'
 import IconPrinter from './printer.svg'
+import IconShop from './shop.svg'
+import IconSoftware from './software.svg'
 import IconSupport from './support.svg'
 import IconTablet from './tablet.svg'
+import IconTime from './time.svg'
 
 export const icons = {
   IconDesktop,
@@ -15,5 +24,14 @@ export const icons = {
   IconTablet,
   IconPrinter,
   IconSupport,
-  IconMaintenance
+  IconMaintenance,
+  IconLocation,
+  IconShop,
+  IconTime,
+  IconPhone,
+  IconCable,
+  IconLink,
+  IconCartridge,
+  IconHarddisk,
+  IconSoftware
 } as const
