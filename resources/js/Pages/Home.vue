cb45a4a75c4afbcbd2e748343337b6c71b8039d1
Alex Valishin <abubekirov.alex@gmail.com>
2026-04-21 20:35:49 +0200

    feat: added cshop component

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import CHero from "@/Components/CHero.vue";
-import CServiceList from "@/Components/CServiceList.vue";
+  import CHero from '@/Components/CHero.vue'
+  import CServiceList from '@/Components/CServiceList.vue'
+  import CShop from '@/Components/CShop.vue'
 </script>
 
 <template>
-    <CHero />
-    <CServiceList />
+  <CHero />
+  <CServiceList />
+  <CShop />
 </template>
