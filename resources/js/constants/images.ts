abaf8e65ee53936ebc68c76307950c1d4ca4cc99
Alex Valishin <abubekirov.alex@gmail.com>
2026-04-21 20:33:31 +0200

    feat: added new images

--- resources/js/constants/images.ts
+++ resources/js/constants/images.ts
@@ -1,9 +1,25 @@
+import imageIpad from '@assets/images/imageIpad.jpg'
+import imageItMaintance from '@assets/images/imageItMaintance.jpg'
+import imageLaptopRepair from '@assets/images/imageLaptopRepair.jpg'
 import imagenDesarrollo from '@assets/images/imagenDesarrollo.jpg'
 import imagenRepara from '@assets/images/imagenRepara.jpg'
 import imagenTienda from '@assets/images/imagenTienda.jpg'
+import imagePcRepair from '@assets/images/imagePcRepair.jpg'
+import imagePhoneRepair from '@assets/images/imagePhoneRepair.jpg'
+import imagePrinterRepair from '@assets/images/imagePrinterRepair.jpg'
+import imageShop from '@assets/images/imageShop.png'
+import imageSupportIt from '@assets/images/imageSupportIt.jpg'
 
 export const images = {
   imagenDesarrollo,
   imagenRepara,
-  imagenTienda
+  imagenTienda,
+  imageIpad,
+  imageLaptopRepair,
+  imagePhoneRepair,
+  imagePcRepair,
+  imagePrinterRepair,
+  imageItMaintance,
+  imageSupportIt,
+  imageShop
 }
